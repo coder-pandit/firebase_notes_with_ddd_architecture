2ace17454cb095024d8b6806991dd011d1dcb9f9
Shiv Kumar <kumarshivsharma12@gmail.com>
2020-06-27 01:28:51 +0530

    Implemented Note From Blco

---
+++ lib/application/notes/note_form/note_form_bloc.dart
@@ -0,0 +1,97 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+import 'package:kt_dart/kt.dart';
+import 'package:meta/meta.dart';
+
+import '../../../domain/notes/i_note_repository.dart';
+import '../../../domain/notes/note.dart';
+import '../../../domain/notes/note_failure.dart';
+import '../../../domain/notes/value_objects.dart';
+import '../../../presentation/notes/note_form/misc/todo_item_presentation_classes.dart';
+
+part 'note_form_bloc.freezed.dart';
+part 'note_form_event.dart';
+part 'note_form_state.dart';
+
+@injectable
+class NoteFormBloc extends Bloc<NoteFormEvent, NoteFormState> {
+  final INoteRepository _noteRepository;
+
+  NoteFormBloc(this._noteRepository);
+
+  @override
+  NoteFormState get initialState => NoteFormState.initial();
+
+  @override
+  Stream<NoteFormState> mapEventToState(
+    NoteFormEvent event,
+  ) async* {
+    yield* event.map(
+      initialized: (e) async* {
+        // when bloc initialized get initial note
+        yield e.initialNoteOption.fold(
+          // if initial note is none then we are in creating mode return unchanged state
+          () => state,
+          // is initially note is found then we are in editing mode so change state accordingly
+          (initialNote) => state.copyWith(
+            note: initialNote,
+            isEditing: true,
+          ),
+        );
+      },
+      bodyChanged: (e) async* {
+        // when note body changed change state with note body changed
+        yield state.copyWith(
+          note: state.note.copyWith(body: NoteBody(e.bodyStr)),
+          saveFailureOrSuccessOption: none(),
+        );
+      },
+      colorChanged: (e) async* {
+        // on note color change, change state with note color changed
+        yield state.copyWith(
+          note: state.note.copyWith(color: NoteColor(e.color)),
+          saveFailureOrSuccessOption: none(),
+        );
+      },
+      todosChanged: (e) async* {
+        // on any todo item change, change the state with todos changed
+        yield state.copyWith(
+          note: state.note.copyWith(
+            // map through each note & get TodoItem from TodoItemPrimitive
+            todos: List3(e.todos.map((primitive) => primitive.toDomain())),
+          ),
+          saveFailureOrSuccessOption: none(),
+        );
+      },
+      saved: (e) async* {
+        Either<NoteFailure, Unit> failureOrSuccess;
+
+        // start saving note
+        yield state.copyWith(
+          isSaving: true,
+          saveFailureOrSuccessOption: none(),
+        );
+
+        // if no error found in note save that
+        if (state.note.failureOption.isNone()) {
+          // if in editing mode update the note else create a new note
+          failureOrSuccess = state.isEditing
+              ? await _noteRepository.update(state.note)
+              : await _noteRepository.create(state.note);
+        }
+
+        // once note saved or failed stop saving & change state accordingly
+        yield state.copyWith(
+          isSaving: false,
+          showErrorMessages: true,
+          saveFailureOrSuccessOption: optionOf(failureOrSuccess),
+        );
+      },
+    );
+  }
+}
